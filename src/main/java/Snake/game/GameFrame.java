fe2d0754695e7f9cc8416c0327e44a7c622fb03f
kavya.lingutla <kavyalingutla@gmail.com>
2023-10-03 13:18:36 +0530

    files added

---
+++ src/main/java/Snake/game/GameFrame.java
@@ -0,0 +1,19 @@
+package Snake.game;
+
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+
+    //constructor for Game Frame
+    GameFrame(){
+
+        this.add(new GamePanel());
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+
+    }
+}
---
+++ src/test/java/Snake/game/SnakeApplicationTests.java
@@ -0,0 +1,13 @@
+package Snake.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
